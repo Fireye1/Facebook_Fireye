2dd8846619e04b55c163977dc1022ff9e48053f1
Valeh <114465476+VaLeh5@users.noreply.github.com>
2022-10-21 10:27:48 +0400

    Valeh.js

---
+++ test.js
@@ -0,0 +1,9 @@
+Sual - 5
+function div2(array) {
+    for (let i=0; i<array.length; i++) {
+    if (array[i]%2 ==0) {
+    return array[i];
+    }
+    const sample_array = [1,2,4,6,71,2,4,9,100];
+    console.log(div2(sample_array));
+    }
